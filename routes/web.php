4f87ca8c659e8d897ed46be22cff4ba9d92cd668
BugsBountyHunter <ahmed.saber20170200@gmail.com>
2024-11-03 00:15:15 +0200

    laracast: 30 days to learn => form

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,26 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view("/", "home");
+Route::resource("jobs", JobController::class, []);
+Route::view("/about", "about");
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs', ["jobs" => $jobs]);
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
-Route::get('/jobs/{id}', function ($id) {
-    return view('job', ["job" => Job::find($id)]);
-});
+// Route::get('/jobs', function () {
+//     $jobs = Job::with('employer')->simplePaginate(3);
+//     return view('jobs', ["jobs" => $jobs]);
+// });
 
-Route::get('/about', function () {
-    return view('about');
-});
+// Route::get('/jobs/{id}', function ($id) {
+//     return view('job', ["job" => Job::find($id)]);
+// });
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
